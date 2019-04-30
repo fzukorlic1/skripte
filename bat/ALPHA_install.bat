0160953f3d3f8f6393843a872ad991d0f9eecfee
hsoljic1 <37188708+hsoljic1@users.noreply.github.com>
2019-04-30 17:32:44 +0200

    Update ALPHA_install.bat

--- bat/ALPHA_install.bat
+++ bat/ALPHA_install.bat
@@ -1,5 +1,5 @@
 mkdir ALPHA
 cd ALPHA
-git clone -b develop https://github.com/si-2019/SI-Alpha-2019.git
+git clone -b dev-node https://github.com/si-2019/SI-Alpha-2019.git
 cd SI-Alpha-2019
 npm install
